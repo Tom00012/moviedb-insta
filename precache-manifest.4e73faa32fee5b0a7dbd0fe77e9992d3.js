e949deff551a8c193c11b63c2b43b0d8a5125f76
Thomas MOREL <pitom01@hotmail.fr>
2020-06-19 17:41:34 +0200

    Updates

---
+++ precache-manifest.4e73faa32fee5b0a7dbd0fe77e9992d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a340574016538edf7cc03ef09f9645",
+    "url": "/moviedb-insta/index.html"
+  },
+  {
+    "revision": "a033fa1b26ef5c28abfc",
+    "url": "/moviedb-insta/static/css/main.5b09f67f.chunk.css"
+  },
+  {
+    "revision": "63a60647153d7503a615",
+    "url": "/moviedb-insta/static/js/2.3ded73e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb-insta/static/js/2.3ded73e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a033fa1b26ef5c28abfc",
+    "url": "/moviedb-insta/static/js/main.c1e57960.chunk.js"
+  },
+  {
+    "revision": "0d765b0a80ebb81479d3",
+    "url": "/moviedb-insta/static/js/runtime-main.cd00e7c6.js"
+  }
+]);
